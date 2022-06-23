4bc97dc0f492b432808190eac1feb0c83f8d1a62
Shambashib <shambashibmajumdar2000@gmail.com>
2022-06-23 12:39:50 +0530

    [ADD] new commits

---
+++ client/src/components/ClientRow.jsx
@@ -0,0 +1,23 @@
+import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
+
+
+export default function ClientRow({ client }) {
+    return (
+        <tr>
+            <td>{client.name}</td>
+            <td>{client.email}</td>
+            <td>{client.phone}</td>
+            <td>
+                <button className="btn btn-danger">
+                    <FaTrash />
+                </button>
+            </td>
+
+        </tr>
+    )
+
+}
